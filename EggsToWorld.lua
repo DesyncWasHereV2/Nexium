0c646e4f1edabd7a90a44e6ad12e5e8e4a09183d
DesyncWasHereV2 <desync.develeoper@gmail.com>
2025-05-11 23:54:52 +0100

    Create EggsToWorld.lua

---
+++ EggsToWorld.lua
@@ -0,0 +1,18 @@
+return {
+    ["Common Egg"] = "Workspace.Worlds.The Overworld.FastTravel.Spawn",
+    ["Spotted Egg"] = "Workspace.Worlds.The Overworld.FastTravel.Spawn",
+    ["Iceshard Egg"] = "Workspace.Worlds.The Overworld.FastTravel.Spawn",
+    ["Spikey Egg"] = "Workspace.Worlds.The Overworld.Islands.Floating Island.Island.Portal.Spawn",
+    ["Crystal Egg"] = "Workspace.Worlds.The Overworld.Islands.Outer Space.Island.Portal.Spawn",
+    ["Magma Egg"] = "Workspace.Worlds.The Overworld.Islands.Outer Space.Island.Portal.Spawn",
+    ["Lunar Egg"] = "Workspace.Worlds.The Overworld.Islands.Twilight.Island.Portal.Spawn",
+    ["Void Egg"] = "Workspace.Worlds.The Overworld.Islands.The Void.Island.Portal.Spawn",
+    ["Hell Egg"] = "Workspace.Worlds.The Overworld.Islands.The Void.Island.Portal.Spawn",
+    ["Nightmare Egg"] = "Workspace.Worlds.The Overworld.Islands.The Void.Island.Portal.Spawn",
+    ["Rainbow Egg"] = "Workspace.Worlds.The Overworld.Islands.Zen.Island.Portal.Spawn",
+    ["Showman Egg"] = "Workspace.Worlds.Minigame Paradise.FastTravel.Spawn",
+    ["Mining Egg"] = "Workspace.Worlds.Minigame Paradise.Islands.Minecart Forest.Island.Portal.Spawn",
+    ["Cyber Egg"] = "Workspace.Worlds.Minigame Paradise.Islands.Robot Factory.Island.Portal.Spawn",
+    ["200M Egg"] = "Workspace.Worlds.The Overworld.FastTravel.Spawn",
+    ["Infinity Egg"] = "Workspace.Worlds.The Overworld.FastTravel.Spawn"
+}
