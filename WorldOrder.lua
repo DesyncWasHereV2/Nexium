1a121a4931520230950a6d1c20bbdc9c18a57865
DesyncWasHereV2 <desync.develeoper@gmail.com>
2025-05-11 23:55:11 +0100

    Update WorldOrder.lua

--- WorldOrder.lua
+++ WorldOrder.lua
@@ -1,14 +1,50 @@
-return = {
-  ["World 1"] = {Path = "game:GetService('Players').LocalPlayer.PlayerGui.ScreenGui.WorldMap.Worlds['The Overworld']['World']", Order = 1},
-  ["Basic Spawn"] = {Path = "game:GetService('Players').LocalPlayer.PlayerGui.ScreenGui.WorldMap.Worlds['The Overworld']['World']", Order = 2},
-  ["Floating Island"] = {Path = "game:GetService('Players').LocalPlayer.PlayerGui.ScreenGui.WorldMap.Worlds['The Overworld'].Islands['Floating Island']", Order = 3},
-  ["Outer Space"] = {Path = "game:GetService('Players').LocalPlayer.PlayerGui.ScreenGui.WorldMap.Worlds['The Overworld'].Islands['Outer Space']", Order = 4},
-  ["Twilight"] = {Path = "game:GetService('Players').LocalPlayer.PlayerGui.ScreenGui.WorldMap.Worlds['The Overworld'].Islands['Twilight']", Order = 5},
-  ["The Void"] = {Path = "game:GetService('Players').LocalPlayer.PlayerGui.ScreenGui.WorldMap.Worlds['The Overworld'].Islands['The Void']", Order = 6},
-  ["Zen"] = {Path = "game:GetService('Players').LocalPlayer.PlayerGui.ScreenGui.WorldMap.Worlds['The Overworld'].Islands['Zen']", Order = 7},
-  ["World 2"] = {Path = "game:GetService('Players').LocalPlayer.PlayerGui.ScreenGui.WorldMap.Worlds['Minigame Paradise']['World']", Order = 8},
-  ["Minigame Spawn"] = {Path = "game:GetService('Players').LocalPlayer.PlayerGui.ScreenGui.WorldMap.Worlds['Minigame Paradise']['World']", Order = 9},
-  ["Dice Island"] = {Path = "game:GetService('Players').LocalPlayer.PlayerGui.ScreenGui.WorldMap.Worlds['Minigame Paradise'].Islands['Dice Island']", Order = 10},
-  ["Minecart Forest"] = {Path = "game:GetService('Players').LocalPlayer.PlayerGui.ScreenGui.WorldMap.Worlds['Minigame Paradise'].Islands['Minecart Forest']", Order = 11},
-  ["Robot Factory"] = {Path = "game:GetService('Players').LocalPlayer.PlayerGui.ScreenGui.WorldMap.Worlds['Minigame Paradise'].Islands['Robot Factory']", Order = 12},
+return {
+    ["World 1"] = {
+        Path = "game:GetService('Players').LocalPlayer.PlayerGui.ScreenGui.WorldMap.Worlds['The Overworld']['World']",
+        Order = 1
+    },
+    ["Basic Spawn"] = {
+        Path = "game:GetService('Players').LocalPlayer.PlayerGui.ScreenGui.WorldMap.Worlds['The Overworld']['World']",
+        Order = 2
+    },
+    ["Floating Island"] = {
+        Path = "game:GetService('Players').LocalPlayer.PlayerGui.ScreenGui.WorldMap.Worlds['The Overworld'].Islands['Floating Island']",
+        Order = 3
+    },
+    ["Outer Space"] = {
+        Path = "game:GetService('Players').LocalPlayer.PlayerGui.ScreenGui.WorldMap.Worlds['The Overworld'].Islands['Outer Space']",
+        Order = 4
+    },
+    ["Twilight"] = {
+        Path = "game:GetService('Players').LocalPlayer.PlayerGui.ScreenGui.WorldMap.Worlds['The Overworld'].Islands['Twilight']",
+        Order = 5
+    },
+    ["The Void"] = {
+        Path = "game:GetService('Players').LocalPlayer.PlayerGui.ScreenGui.WorldMap.Worlds['The Overworld'].Islands['The Void']",
+        Order = 6
+    },
+    ["Zen"] = {
+        Path = "game:GetService('Players').LocalPlayer.PlayerGui.ScreenGui.WorldMap.Worlds['The Overworld'].Islands['Zen']",
+        Order = 7
+    },
+    ["World 2"] = {
+        Path = "game:GetService('Players').LocalPlayer.PlayerGui.ScreenGui.WorldMap.Worlds['Minigame Paradise']['World']",
+        Order = 8
+    },
+    ["Minigame Spawn"] = {
+        Path = "game:GetService('Players').LocalPlayer.PlayerGui.ScreenGui.WorldMap.Worlds['Minigame Paradise']['World']",
+        Order = 9
+    },
+    ["Dice Island"] = {
+        Path = "game:GetService('Players').LocalPlayer.PlayerGui.ScreenGui.WorldMap.Worlds['Minigame Paradise'].Islands['Dice Island']",
+        Order = 10
+    },
+    ["Minecart Forest"] = {
+        Path = "game:GetService('Players').LocalPlayer.PlayerGui.ScreenGui.WorldMap.Worlds['Minigame Paradise'].Islands['Minecart Forest']",
+        Order = 11
+    },
+    ["Robot Factory"] = {
+        Path = "game:GetService('Players').LocalPlayer.PlayerGui.ScreenGui.WorldMap.Worlds['Minigame Paradise'].Islands['Robot Factory']",
+        Order = 12
+    }
 }
