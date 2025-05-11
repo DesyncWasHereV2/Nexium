5491136eb564c95a3dc14e6734f2b25e20e6b501
DesyncWasHereV2 <desync.develeoper@gmail.com>
2025-05-11 23:55:49 +0100

    Update EggOrder.lua

--- EggOrder.lua
+++ EggOrder.lua
@@ -1,6 +1,20 @@
-return = {
-    ["Common Egg"] = 1, ["Spotted Egg"] = 2, ["IceShard Egg"] = 3, ["Spikey Egg"] = 4,
-    ["Magma Egg"] = 5, ["Crystal Egg"] = 6, ["Lunar Egg"] = 7, ["Void Egg"] = 8,
-    ["Hell Egg"] = 9, ["Nightmare Egg"] = 10, ["Rainbow Egg"] = 11, ["Showman Egg"] = 12,
-    ["Mining Egg"] = 13, ["Cyber Egg"] = 14, ["Game Egg"] = 15 , ["Underworld Egg"] = 16, ["200M Egg"] = 17 ["Infinity Egg"] = 18,
+return {
+    ["Common Egg"] = 1,
+    ["Spotted Egg"] = 2,
+    ["IceShard Egg"] = 3,
+    ["Spikey Egg"] = 4,
+    ["Magma Egg"] = 5,
+    ["Crystal Egg"] = 6,
+    ["Lunar Egg"] = 7,
+    ["Void Egg"] = 8,
+    ["Hell Egg"] = 9,
+    ["Nightmare Egg"] = 10,
+    ["Rainbow Egg"] = 11,
+    ["Showman Egg"] = 12,
+    ["Mining Egg"] = 13,
+    ["Cyber Egg"] = 14,
+    ["Game Egg"] = 15,
+    ["Underworld Egg"] = 16,
+    ["200M Egg"] = 17,
+    ["Infinity Egg"] = 18
 }
