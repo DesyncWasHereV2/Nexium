5c28e0b32d1cf63827382541a9f79829ec4690e9
DesyncWasHereV2 <desync.develeoper@gmail.com>
2025-05-11 23:46:01 +0100

    Update EggOrder.lua

--- EggOrder.lua
+++ EggOrder.lua
@@ -1,4 +1,4 @@
-local order = {
+return = {
     ["Common Egg"] = 1, ["Spotted Egg"] = 2, ["IceShard Egg"] = 3, ["Spikey Egg"] = 4,
     ["Magma Egg"] = 5, ["Crystal Egg"] = 6, ["Lunar Egg"] = 7, ["Void Egg"] = 8,
     ["Hell Egg"] = 9, ["Nightmare Egg"] = 10, ["Rainbow Egg"] = 11, ["Showman Egg"] = 12,
