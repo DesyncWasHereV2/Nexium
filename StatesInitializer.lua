059adbcf90e76146a1d788cfaa3d67a2ae9cc83d
DesyncWasHereV2 <desync.develeoper@gmail.com>
2025-05-14 00:11:08 +0100

    Update StatesInitializer.lua

--- StatesInitializer.lua
+++ StatesInitializer.lua
@@ -3,13 +3,21 @@ return function(states)
 
     for key, default in pairs(defaultStates) do
         if not states[key] then
+            print("[StatesInitializer] Missing state:", key, "- Adding default.")
             states[key] = default
         else
             for subKey, subDefault in pairs(default) do
-                if not states[key][subKey] then
+                if states[key][subKey] == nil then
+                    print(string.format("[StatesInitializer] Missing subKey '%s' in state '%s' - Adding default.", subKey, key))
                     states[key][subKey] = subDefault
                 end
             end
         end
+    end
+
+    -- Final print to confirm structure
+    print("[StatesInitializer] Final States Table:", states)
+    for k, v in pairs(states) do
+        print("  ", k, "=", v)
     end
 end
